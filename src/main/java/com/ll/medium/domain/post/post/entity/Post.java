0e88edcd73c7e0476da399577db50fcacc3b0db6
jhs512 <jangka512@gmail.com>
2024-01-08 15:46:26 +0900

    work

--- src/main/java/com/ll/medium/domain/post/post/entity/Post.java
+++ src/main/java/com/ll/medium/domain/post/post/entity/Post.java
@@ -45,9 +45,22 @@ public class Post extends BaseEntity {
     private PostDetail detailBody;
 
     private boolean published;
+
     @Setter(PROTECTED)
     private long hit;
+
+    @Setter(PROTECTED)
+    private long likesCount;
+
     private int minMembershipLevel;
+
+    public void increaseLikesCount() {
+        likesCount++;
+    }
+
+    private void decreaseLikesCount() {
+        likesCount--;
+    }
 
     public void increaseHit() {
         hit++;
@@ -62,6 +75,8 @@ public class Post extends BaseEntity {
                 .post(this)
                 .member(member)
                 .build());
+
+        increaseLikesCount();
     }
 
     public boolean hasLike(Member member) {
@@ -70,7 +85,11 @@ public class Post extends BaseEntity {
     }
 
     public void deleteLike(Member member) {
-        likes.removeIf(postLike -> postLike.getMember().equals(member));
+        boolean removed = likes.removeIf(postLike -> postLike.getMember().equals(member));
+
+        if (removed) {
+            decreaseLikesCount();
+        }
     }
 
     public PostComment writeComment(Member actor, String body) {
